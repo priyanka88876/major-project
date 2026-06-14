b2a6df43abaa02bef5e832ee87eff6d42f5c4df7
Priyanka pandey <pp6934270@gmail.com>
2026-06-14 19:08:05 +0530

    added listings model and initialised

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const  express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_UGL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,22 +18,29 @@ async function main() {
     await mongoose.connect(MONGO_UGL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "views"));
+
 app.get("/", (req,res) => {
     res.send("Hi ,I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute,Goa",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", {allListings});
 });
+//app.get("/testListing", async (req, res) => {
+  //  let sampleListing = new Listing({
+    //    title: "My New Villa",
+      //  description: "By the beach",
+        //price: 1200,
+        //location: "Calangute,Goa",
+        //country: "India",
+    //});
+    //await sampleListing.save();
+    //console.log("sample was saved");
+    //res.send("successful testing");
+//});
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -8,6 +8,7 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
+        url:{
         type: String,
         default:
          "https://in.images.search.yahoo.com/search/images?p=images.unsplash.com%2Fphoto&fr=mcafee&type=E210IN826G0&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1625505826533-5c80aca7d157%3Fixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8MTR8fGF0YXJkZWNlciUyMHBsYXlhfGVufDB8fDB8fHww%26fm%3Djpg%26q%3D60%26w%3D3000#id=36&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1625505826533-5c80aca7d157%3Fixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8MTR8fGF0YXJkZWNlciUyMHBsYXlhfGVufDB8fDB8fHww%26fm%3Djpg%26q%3D60%26w%3D3000&action=click",
@@ -15,6 +16,7 @@ const listingSchema = new Schema({
         set: (v) => v === ""
          ? "https://in.images.search.yahoo.com/search/images?p=images.unsplash.com%2Fphoto&fr=mcafee&type=E210IN826G0&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1625505826533-5c80aca7d157%3Fixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8MTR8fGF0YXJkZWNlciUyMHBsYXlhfGVufDB8fDB8fHww%26fm%3Djpg%26q%3D60%26w%3D3000#id=36&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1625505826533-5c80aca7d157%3Fixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8MTR8fGF0YXJkZWNlciUyMHBsYXlhfGVufDB8fDB8fHww%26fm%3Djpg%26q%3D60%26w%3D3000&action=click"
         : v,
+        }
     },
     price: Number,
     location: String,
